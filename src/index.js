c9e3fe6fffee7d98c94b07fa4a45bf093cf77fdc
Thailer <thailer.palmer@gmail.com>
2023-03-03 15:46:19 -0500

    Lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit',( event) => {
+  event.preventDefault()
+  postIt(event.target.children[1].value)
+  form.reset()
+
+})
 });
+
+function postIt(list){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',listDelete)
+   btn.textContent = 'X'
+  p.textContent = `${list} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function listDelete(event){
+event.target.parentNode.remove()
+}
